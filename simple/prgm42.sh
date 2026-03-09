9d10f5989ac34f2b55929cca29db91e17a1683ae
NRZ-10 <niranjan.zr710@gmail.com>
2026-03-09 13:56:26 +0530

    done

---
+++ simple/prgm42.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Enter a number: " num
+
+if [ $num -gt 0 ]; then
+	echo "Number is positive."
+elif [ $num -lt 0 ]; then
+	echo "Number is negative."
+else
+	echo "Number is zero."
+fi
+
